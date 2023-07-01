0dfc810f4336eff1f8dc6a4f57d016a10ad2126f
SAHANI <sss921613@tatatechnologies.com>
2023-07-01 23:55:58 +0530

    params passing and page UI

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,123 @@
+"use client";
+// ! "use client" Convert any component into client component now using "use client" you can access your window object,useEffect,useState etc
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignupPage() {
-    return <div>Sign Up</div>;
-  }
-  
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {
+    //
+  };
+  return (
+    <section>
+      <div className="flex items-center justify-center  bg-slate-900 px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+        <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+          <h2 className="text-center text-2xl font-bold leading-tight text-white">
+            Sign up to create account
+          </h2>
+
+          <form action="#" method="POST" className="mt-8">
+            <div className="space-y-5">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="text-base font-medium text-white"
+                >
+                  {" "}
+                  User Name{" "}
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="text"
+                    placeholder="User Name"
+                    value={user.username}
+                    id="name"
+                    onChange={(e) =>
+                      setUser({ ...user, username: e.target.value })
+                    }
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="text-base font-medium text-white"
+                >
+                  {" "}
+                  Email address{" "}
+                </label>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="email"
+                    placeholder="Email"
+                    value={user.email}
+                    id="email"
+                    onChange={(e) =>
+                      setUser({ ...user, email: e.target.value })
+                    }
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="text-base font-medium text-white"
+                  >
+                    {" "}
+                    Password{" "}
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                    type="password"
+                    placeholder="Password"
+                    value={user.password}
+                    id="password"
+                    onChange={(e) =>
+                      setUser({ ...user, password: e.target.value })
+                    }
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </form>
+          <div className="mt-3 space-y-3">
+            <button
+              onClick={onSignup}
+              type="button"
+              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+            >
+              <span className="mr-2 inline-block">
+                <svg
+                  className="h-6 w-6 text-rose-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
+                </svg>
+              </span>
+              Sign up
+            </button>
+          </div>
+          <br/>
+          <Link href="/login"> Visit Login Page </Link>
+          <br/>
+          <p className="text-white"> {JSON.stringify(user)}</p>
+        </div>
+      </div>
+     
+    </section>
+  );
+}
